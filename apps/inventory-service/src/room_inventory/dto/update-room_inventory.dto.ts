b772251942112f133e57e530aa4d1653e5d8facd
TuanNx <tuan9864543@gmail.com>
2026-05-14 14:30:27 +0700

    add to be & fe folder

--- apps/inventory-service/src/room_inventory/dto/update-room_inventory.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-
-import { CreateRoomInventoryDto } from './create-room_inventory.dto';
-
-export class UpdateRoomInventoryDto extends PartialType(CreateRoomInventoryDto) {}
