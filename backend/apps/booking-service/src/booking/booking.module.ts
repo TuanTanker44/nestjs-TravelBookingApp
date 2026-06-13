04403a82dbdf40b4641188ce6e6962a95021a80e
TuanNx <tuan9864543@gmail.com>
2026-06-13 09:00:49 +0700

    booking: add index to entity, add controller, compelete module

--- backend/apps/booking-service/src/booking/booking.module.ts
+++ backend/apps/booking-service/src/booking/booking.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { ScheduleModule } from '@nestjs/schedule';
+import { Booking } from './entities/booking.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([Booking])],
   controllers: [BookingController],
   providers: [BookingService],
 })
--- backend/apps/booking-service/src/booking/entities/booking.entity.ts
+++ backend/apps/booking-service/src/booking/entities/booking.entity.ts
@@ -5,6 +5,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   OneToMany,
+  Index,
 } from 'typeorm';
 import { BookingStatus } from '../enums/status.enum';
 import { BookingPaymentStatus } from '../enums/payment-status.enum';
@@ -12,30 +13,39 @@ import { BookingRoom } from '../../booking_room/entities/booking_room.entity';
 
 @Entity('bookings')
 export class Booking {
+  @Index('UNIQUE_BOOKING_ID', {
+    unique: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
   @Column()
+  @Index()
   userId!: string;
 
   @Column()
+  @Index()
   hotelId!: string;
 
   @Column({ type: 'date' })
+  @Index()
   checkInDate!: Date;
 
   @Column({ type: 'date' })
+  @Index()
   checkOutDate!: Date;
 
   @Column({ type: 'decimal', precision: 12, scale: 2 })
   totalAmount!: number;
 
+  @Index()
   @Column({
     type: 'enum',
     enum: BookingStatus,
   })
   status!: BookingStatus;
 
+  @Index()
   @Column({
     type: 'enum',
     enum: BookingPaymentStatus,
@@ -43,6 +53,7 @@ export class Booking {
   paymentStatus!: BookingPaymentStatus;
 
   @CreateDateColumn()
+  @Index()
   createdAt?: Date;
 
   @UpdateDateColumn()
